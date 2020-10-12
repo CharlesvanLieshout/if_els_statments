0b1df5a525d7523712267cda4c3121dcc65af538
CharlesvanLieshout <66746563+CharlesvanLieshout@users.noreply.github.com>
2020-10-12 10:30:37 +0200

    Add files via upload

---
+++ If_else statements/script.js
@@ -0,0 +1,24 @@
+const Age = 10
+if (Age >= 18) {
+    console.log ("Welkom, kom binnen");
+} else {
+    console.log ("Sorry, je bent te jong");
+}
+
+const isFemale = false
+if (isFemale == true) {
+console.log ("Welkom");
+} else {
+    console.log ("Vandaag is ladiesnight. Kom morgen terug");
+}
+
+const driverStatus = "bob"
+
+if (driverStatus == "bob") {
+    console.log ("Jij bent de bob. Jij mag rijden");
+}
+    else {
+        console.log ("Laat iemand anders rijden");
+    }
+
+
